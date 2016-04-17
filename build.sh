5b28e5268b1a1cb18749f742f99204c8d4869124
StartEnd <858965877@qq.com>
2016-04-17 11:27:27 +0800

    k

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ git config --global user.email "858965877@qq.com" #填入GitHub的邮箱地址
 git config --global user.name "StartEnd" #填入GitHub的用户名
 git add --all .
 git commit -m "Travis CI Auto Builder" #自动构建后的内容将全部以此信息提交
-git push --quiet --force https://${0f50c7816694b46f309ef9599dab2bfec9a3b5a1}@github.com/StartEnd/StartEnd.io.git master  #自动构建后的内容将全部以此信息提交
+git push --quiet --force https://${0f50c7816694b46f309ef9599dab2bfec9a3b5a1}@github.com/StartEnd/StartEnd.github.io.git master  #自动构建后的内容将全部以此信息提交
 ##curl --connect-timeout 20 --max-time 30 -s http://远端服务器URL/webhook.php?_=${NOTIFY_TOKEN} #服务器Webhook 将在下文介绍
